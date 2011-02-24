fbb4173b6206f31406be67a70d8c73f1e7151529
Bartlomiej Kozal <bartlomiejkozal@gmail.com>
2011-02-24 17:11:26 +0100

    Added settings as function argument

--- to_modal.js
+++ to_modal.js
@@ -1,46 +1,55 @@
 (function($) {
   $.fn.toModal = function(options) {
 
-    // TODO: Add default settings
-    // var settings = {
-    // ...
-    // };
+    var settings = {
+      opacity: '0.85',
+      zIndex: 100,
+      showOnLoad: false,
+      openElement: ".toModalOpen",
+      closeElement: ".toModalClose",
+      overlayElement: "#toModalOverlay"
+    };
 
     return this.each(function() {
 
-      // if (options) {
-      //   $.extend(settings, options);
-      // }
+      if (options) {
+        $.extend(settings, options);
+      }
 
       var popup = $(this);
-      var overlay = $('<div id="toModalOverlay" />').prependTo('body');
+      var overlay = $('<div id="' + settings.overlayElement + '" />').prependTo('body');
       var modal = [popup, overlay];
+      
+      var isDisplay = function() {
+        return (settings.showOnLoad) ? 'block' : 'none';
+      }
 
       overlay.css({
-        'z-index': 100,
-        'background-color': '#000',
-        'opacity': '0.85',
-        'position': 'fixed',
-        'top': 0, 'right': 0, 'bottom': 0, 'left': 0,
-        'display': 'none'
+        zIndex: settings.zIndex,
+        backgroundColor: '#000',
+        opacity: settings.opacity,
+        position: 'fixed',
+        top: 0, right: 0, bottom: 0, left: 0,
+        display: isDisplay
       });
 
       popup.css({
-        'z-index': 101,
-        'margin-top': -(popup.height() / 2),
-        'margin-left': -(popup.width() / 2),
-        'position': 'fixed',
-        'top': '50%', 'left': '50%',
-        'display': 'none'
+        zIndex: settings.zIndex += 1,
+        marginTop: -(popup.height() / 2),
+        marginLeft: -(popup.width() / 2),
+        position: 'fixed',
+        top: '50%', left: '50%',
+        display: isDisplay
       });
-      
-      $(".toModalClose").click(function() {
-        $.each(modal, function() {this.hide()});
-      });
-      
-      $(".toModalOpen").click(function() {
+
+      $(settings.openElement).click(function() {
         $.each(modal, function() {this.show()});
       });
+
+      $(settings.closeElement).click(function() {
+        $.each(modal, function() {this.hide()});
+      });
+
 
     });
   };
